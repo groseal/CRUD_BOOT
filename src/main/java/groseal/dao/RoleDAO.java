99dc6852d8617d6d481db5120a35054a71d33c08
Громов Сергей <59777269+groseal@users.noreply.github.com>
2021-05-25 22:46:38 +0300

    Update RoleDAO.java

--- src/main/java/groseal/dao/RoleDAO.java
+++ src/main/java/groseal/dao/RoleDAO.java
@@ -3,6 +3,8 @@ package groseal.dao;
 import groseal.models.Role;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RoleDAO {
 
@@ -11,5 +13,7 @@ public interface RoleDAO {
     Role getRoleByName(String role);
 
     void deleteRole(Long id);
+
+    List<Role> getAllRoles();
 
 }
