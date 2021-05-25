e16948b84d8e7eedeb7c0a133d085744a7eefb4b
Громов Сергей <59777269+groseal@users.noreply.github.com>
2021-05-25 22:47:14 +0300

    Update RoleDAOImpl.java

--- src/main/java/groseal/dao/RoleDAOImpl.java
+++ src/main/java/groseal/dao/RoleDAOImpl.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 
 @Repository
 public class RoleDAOImpl implements RoleDAO {
@@ -29,4 +30,10 @@ public class RoleDAOImpl implements RoleDAO {
     public void deleteRole(Long id) {
         entityManager.remove(getRoleById(id));
     }
+
+    @Override
+    public List<Role> getAllRoles() {
+        return entityManager.createQuery("SELECT r FROM Role r", Role.class).getResultList();
+    }
+
 }
